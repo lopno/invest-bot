e1db3f436af866b17a87f897bb8c2c0b92ec1b1a
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-03-06 18:18:52 +0100

    Added favicon

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.listen(3000);
