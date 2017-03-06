5b95035a35dba93a5f1c58377939ef270e948b56
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-03-06 19:08:24 +0100

    Copied invest-watch logic and made PoC end-point

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const app = express();
 
+const valuationUtils = require('./lib/utils/valuation');
+const fundTypeConstants = require('./lib/constant/fundType');
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+  return valuationUtils.getValuation(fundTypeConstants.balanced)
+    .then((valuation) => res.send(JSON.stringify(valuation)));
+});
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
