98bda6697e08c5cd31f4541b10faa0a5ce5928aa
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-04-13 15:15:05 +0200

    Save and load valuations from database

---
+++ integrationTest/database/database.integration.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const database = require('../../lib/database/database');
+
+/* global describe, it */
+
+if (process.env.DATABASE_URL) {
+  describe('database', function () {
+    describe('getLatestValuations', function () {
+      it('should return 2 valuations if given 2 as input', function (done) {
+        const count = 2;
+        database.getLatestValuations(count)
+          .then((result) => {
+            assert.lengthOf(result, count, `expected ${count} rows as result`);
+          })
+          .then(() => done())
+          .catch(done);
+      });
+    });
+  });
+} else {
+  throw new Error('DATABASE_URL not defined');
+}
