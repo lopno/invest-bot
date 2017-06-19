f9333ceb641a7099f05576e8e25ef5ec7630c0c6
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-06-19 23:04:14 +0200

    Switched to Ava for testing and assertions :rocket:

--- integrationTest/database/database.integration.js
+++ integrationTest/database/database.integration.js
@@ -1,23 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies, func-names, prefer-arrow-callback */
-const assert = require('chai').assert;
+/* eslint-disable import/no-extraneous-dependencies */
+const test = require('ava');
 const database = require('../../lib/database/database');
 
-/* global describe, it */
-
-if (process.env.DATABASE_URL) {
-  describe('database', function () {
-    describe('getLatestValuations', function () {
-      it('should return 2 valuations if given 2 as input', function (done) {
-        const count = 2;
-        database.getLatestValuations(count)
-          .then((result) => {
-            assert.lengthOf(result, count, `expected ${count} rows as result`);
-          })
-          .then(() => done())
-          .catch(done);
-      });
+test('getLatestValuations should return 2 valuations if given 2 as input', (t) => {
+  const count = 2;
+  return database.getLatestValuations(count)
+    .then((result) => {
+      t.is(result.length, count, `expected ${count} rows as result`);
     });
-  });
-} else {
-  throw new Error('DATABASE_URL not defined');
-}
+});
