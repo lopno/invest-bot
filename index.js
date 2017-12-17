3b6c2605109a356f94f56fe322601728392f3499
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-12-17 17:35:30 +0100

    Refactor to worker using botkit, refactor db env vars (#32)

--- index.js
+++ index.js
@@ -1,87 +1,59 @@
 /* eslint-disable no-console */
-const SlackBot = require('slackbots');
-const http = require('http');
-const express = require('express');
-const favicon = require('serve-favicon');
-
+const Botkit = require('botkit');
 const graphAction = require('./lib/action/graph');
 const slackMessageParse = require('./lib/parse/slackMessage');
 const messageUtils = require('./lib/utils/message');
 const valuationUtils = require('./lib/utils/valuation');
 const actionTypes = require('./lib/constant/action');
 
-const app = express();
-var investBotUserId = null; // eslint-disable-line no-var
-
-// every 5 minutes (300000)
-setInterval(() => http.get('http://protected-savannah-66517.herokuapp.com'), 300000);
-
-app.get('/', (req, res) => res.sendStatus(200));
-
-app.use(favicon(`${__dirname}/public/favicon.ico`));
-
-app.listen(process.env.PORT || 3000);
-
-const investBot = new SlackBot({
-  token: process.env.SLACK_TOKEN,
-  name: 'Invest Bot',
+// init slack bot
+const investBot = Botkit.slackbot({
+  retry: 'Infinity',
+  debug: false
 });
 
-// more information about additional params https://api.slack.com/methods/chat.postMessage
-const params = {
-  icon_emoji: ':ok_hand:',
-};
+investBot
+  .spawn({
+    token: process.env.SLACK_TOKEN
+  })
+  .startRTM((err) => {
+    if (err) {
+      throw new Error(err);
+    } else {
+      console.log('Invest bot is up and running...');
+    }
+  });
 
-investBot.on('start', () => {
-  investBot.getUser(process.env.BOT_NAME || 'investbot')
-    .then((user) => {
-      investBotUserId = user.id;
-    });
-});
 
-function getChannelById(channelId) {
-  // console.log('channels', channels);
-  console.log('channelId', channelId);
-  const channelRes = investBot.channels.filter(item => (item.id === channelId))[0];
-  console.log('found this channel: ', channelRes.name);
-  return channelRes.name;
-}
+investBot.on('direct_mention', (bot, message) => {
+  console.log(message);
 
-// all ingoing events https://api.slack.com/rtm
-investBot.on('message', (data) => {
-  if (investBotUserId
-    && data.type === 'message'
-    && data.text.includes(`<@${investBotUserId}>`)) {
-    const channel = getChannelById(data.channel);
-    const parsedMessage = slackMessageParse.parseSlackMessage(data.text);
-    console.log('Message: ', data.text);
-    console.log('Parsed Message: ', parsedMessage);
+  const parsedMessage = slackMessageParse.parseSlackMessage(message.text);
+  console.log('Message: ', message.text);
+  console.log('Parsed Message: ', parsedMessage);
 
-    if (parsedMessage.action === actionTypes.latestValuation) {
-      valuationUtils.getValuation(parsedMessage.funds)
-        .then(valuations =>
-          valuations.forEach(valuation =>
-            investBot.postMessageToChannel(
-              channel,
-              messageUtils.formatValuation(
-                valuation, parsedMessage.sharesCount, valuation.fundType),
-              params
-            )
+  if (parsedMessage.action === actionTypes.latestValuation) {
+    valuationUtils.getValuation(parsedMessage.funds)
+      .then(valuations =>
+        valuations.forEach(valuation =>
+          bot.reply(
+            message,
+            messageUtils.formatValuation(
+              valuation, parsedMessage.sharesCount, valuation.fundType)
           )
         )
-        .catch(error => console.log('error: ', error));
-    } else if (parsedMessage.action === actionTypes.graph) {
-      // TODO: support more funds in one graph later
-      // TODO: rename parsedMessage.sharesCount
-      graphAction.graphFund(parsedMessage.funds[0], parseInt(parsedMessage.sharesCount, 10))
-        .then((graphLink) => {
-          investBot.postMessageToChannel(
-            channel,
-            graphLink,
-            params
-          );
-        })
-        .catch(error => console.log(JSON.stringify(error)));
-    }
+      )
+      .catch(error => console.log('error: ', error));
+  } else if (parsedMessage.action === actionTypes.graph) {
+    // TODO: support more funds in one graph later
+    // TODO: rename parsedMessage.sharesCount
+    graphAction.graphFund(parsedMessage.funds[0], parseInt(parsedMessage.sharesCount, 10))
+      .then((graphLink) => {
+        bot.reply(
+          message,
+          graphLink
+        );
+      })
+      .catch(error => console.log(JSON.stringify(error)));
   }
 });
