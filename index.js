3be9463e148042d2899458ae2ca3facfa9332d6a
Vedran Perendija <vedran.perendija@gmail.com>
2018-02-20 20:03:23 +0100

    possible to direct message with investbot (#39)

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ investBot
     }
   });
 
-
-investBot.on('direct_mention', (bot, message) => {
+function handleMessage(bot, message) {
   console.log(message);
 
   const parsedMessage = slackMessageParse.parseSlackMessage(message.text);
@@ -35,59 +34,60 @@ investBot.on('direct_mention', (bot, message) => {
 
   if (parsedMessage.action === actionTypes.latestValuation) {
     valuationUtils.getValuation(parsedMessage.funds)
-      .then(valuations =>
-        valuations.forEach(valuation =>
-          bot.reply(
-            message,
-            messageUtils.formatValuation(
-              valuation, parsedMessage.sharesCount, valuation.fundType)
+        .then(valuations =>
+          valuations.forEach(valuation =>
+            bot.reply(
+              message,
+              messageUtils.formatValuation(
+                valuation, parsedMessage.sharesCount, valuation.fundType)
+            )
           )
         )
-      )
-      .catch(error => console.log('error: ', error));
+        .catch(error => console.log('error: ', error));
   } else if (parsedMessage.action === actionTypes.help) {
     bot.reply(
-      message,
-      messageUtils.getHelp()
-    )
-    .catch(error => console.log(JSON.stringify(error)));
+        message,
+        messageUtils.getHelp()
+        )
+        .catch(error => console.log(JSON.stringify(error)));
   } else if (parsedMessage.action === actionTypes.graph) {
-    // TODO: support more funds in one graph later
-    // TODO: rename parsedMessage.sharesCount
+      // TODO: support more funds in one graph later
+      // TODO: rename parsedMessage.sharesCount
     const sharesCountInt = parseInt(parsedMessage.sharesCount, 10);
     const sharesCount = sharesCountInt === 0 ? 10 : sharesCountInt;
     graphAction.graphFund(parsedMessage.funds[0], sharesCount)
-      .then((graphLink) => {
-        bot.reply(
-          message,
-          graphLink
-        );
-      })
-      .catch(error => console.log(JSON.stringify(error)));
-  } else if (parsedMessage.action === actionTypes.invested) {
-    investmentUtils.getInvestment(parsedMessage.funds)
-      .then(investments =>
-        investments.forEach(invested =>
+        .then((graphLink) => {
           bot.reply(
             message,
-            messageUtils.formatInvestment(
-              invested,
-              invested.fundType)
+            graphLink
+          );
+        })
+        .catch(error => console.log(JSON.stringify(error)));
+  } else if (parsedMessage.action === actionTypes.invested) {
+    investmentUtils.getInvestment(parsedMessage.funds)
+        .then(investments =>
+          investments.forEach(invested =>
+            bot.reply(
+              message,
+              messageUtils.formatInvestment(
+                invested,
+                invested.fundType)
+            )
           )
         )
-      )
-      .catch(error => console.log(JSON.stringify(error)));
+        .catch(error => console.log(JSON.stringify(error)));
   } else if (parsedMessage.action === actionTypes.total) {
     investmentUtils.getInvestment(parsedMessage.funds)
-     .then(investment =>
-         bot.reply(
-           message,
-           messageUtils.getTotalInvestment(
-             investment,
-             investment.fundType,
-             parsedMessage.action)
-         )
-     )
-     .catch(error => console.log(JSON.stringify(error)));
+        .then(investment =>
+          bot.reply(
+            message,
+            messageUtils.getTotalInvestment(
+              investment,
+              investment.fundType,
+              parsedMessage.action)
+          )
+        )
+        .catch(error => console.log(JSON.stringify(error)));
   }
-});
+}
+investBot.on('direct_message,direct_mention', handleMessage);
